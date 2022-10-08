8396d13d28d483361dccf9d9dac8b26e70c64989
trevin-livele <liveletrevin6@gmail.com>
2022-10-08 16:43:36 +0300

    ✨ {refactor} added new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
